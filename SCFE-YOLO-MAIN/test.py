7789fbf2a5dc2006d7d82a400c214296becb6fc3
HuggingTiger <91590387+zzwyx99@users.noreply.github.com>
2026-07-14 15:22:05 +0800

    Delete SCFE-YOLO-MAIN directory

--- SCFE-YOLO-MAIN/test.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-from custom_blocks import register_custom_modules
-register_custom_modules()
-from ultralytics import YOLO
-
-if __name__ == '__main__':
-    model = YOLO("SCFEYOLO.pt")
-    results = model.val(data="VisDrone.yaml", imgsz=640, name="custom_-noFElaocangku", split="val")
--- SCFE-YOLO-MAIN/trainSCFEYOLO.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-from custom_blocks import register_custom_modules
-register_custom_modules()
-if __name__ == '__main__':
-    model = YOLO("configs/SCFE-YOLO.yaml")
-    model.train(data="VisDrone.yaml", epochs=500, batch=8
-    , imgsz=640, deterministic=False,name="SCFE-YOLO",pretrained=False)
